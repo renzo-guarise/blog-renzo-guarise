a59ce975ba7ac8dbb6178b6ca97694e6843fe021
Renzo Guarise <rguarise@MacBook-Pro-de-Renzo.local>
2025-04-07 20:18:34 -0300

    Fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
-  basePath: '/renzo-guarise', // Replace with your repository name
+  basePath: 'https://renzo-guarise.github.io', // Replace with your repository name
   images: {
     unoptimized: true, // Required for static export
   },
